dea2f87e2c068f84e6436986dca488dc3768c1dc
Colson Arthur <arthur.colson@etu.iut-tlse3.fr>
2024-11-22 10:45:44 +0100

    modif

---
+++ code/pages/joueur.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ultimate Manager - Joueur</title>
+    <link rel="stylesheet" href="./../css/global.css">
+    <link rel="stylesheet" href="./../css/header.css">
+    <link rel="stylesheet" href="./../css/joueur.css">
+</head>
+
+<body>
+    <div class="header">
+        <?php include './../headfoot/header.html';?>
+    </div>
+    <main>
+        <h1>Gestion des Joueurs</h1>
+        <section>
+            <form>
+                <label for="nom">Nom du Joueur :</label>
+                <input type="text" id="nom" name="nom" required>
+                
+                <label for="equipe">Équipe :</label>
+                <input type="text" id="equipe" name="equipe" required>
+                
+                <button type="submit">Ajouter Joueur</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
